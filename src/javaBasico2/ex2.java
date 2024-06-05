61a8ff2f96eae94e90a6cd5c0f24dfdc05d2034c
Nicollas <nsilvademoraes@gmail.com>
2024-06-05 14:46:38 -0300

    Exercicios Ex2-2

---
+++ src/javaBasico2/ex2.java
@@ -0,0 +1,32 @@
+package javaBasico2;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número para verificação");
+        int numero = entrada.nextInt();
+
+        String parOuImpar;
+
+        if (numero % 2 == 0) {
+            parOuImpar = "Par";
+        } else {
+            parOuImpar = "Impar";
+        }
+
+        if (numero == 0) {
+            System.out.println("O número 0 é neutro");
+        } else if (numero > 0) {
+            System.out.println("O número " + numero + " é " + parOuImpar + " e positivo!");
+        } else {
+            if (parOuImpar.equals("par")) {
+                System.out.println("O número " + numero + " é " + parOuImpar + " e negativo!");
+            } else {
+                System.out.println("O número " + numero + " é " + parOuImpar + " e negativo!");
+            }
+        }
+    }
+}
