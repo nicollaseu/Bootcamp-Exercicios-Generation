ffd2941062f16e1d0f5281a224ca40a528de1cfb
Nicollas <nsilvademoraes@gmail.com>
2024-06-05 19:41:00 -0300

    Exercicios Ex7-2

---
+++ src/javaBasico2/ex7.java
@@ -0,0 +1,45 @@
+package javaBasico2;
+import java.util.Scanner;
+
+public class ex7 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        float valor1, valor2, resultado;
+        int operacao;
+        System.out.println("=== CALCULADORA ===");
+        System.out.println("Digite o primeiro valor entre 1 e 4: ");
+        valor1 = entrada.nextFloat();
+        System.out.println("Digite o segundo valor entre 1 e 4: ");
+        valor2 = entrada.nextFloat();
+
+        if (valor1 >= 1 && valor1 <= 4 && valor2 >= 1 && valor2 <= 4) {
+            System.out.println(" === Escolha o número da operação ===");
+            System.out.println("SOMA[1] | SUBTRAÇÃO[2] | MULTIPLICAÇÃO[3] | DIVISÃO[4] ");
+            operacao = entrada.nextInt();
+            switch (operacao) {
+                case 1 -> {
+                    resultado = valor1 + valor2;
+                    System.out.println(valor1 + " + " + valor2 + " = " + resultado);
+                }
+                case 2 -> {
+                    resultado = valor1 - valor2;
+                    System.out.println(valor1 + " - " + valor2 + " = " + resultado);
+                }
+                case 3 -> {
+                    resultado = valor1 * valor2;
+                    System.out.println(valor1 + " + " + valor2 + " = " + resultado);
+                }
+                case 4 -> {
+                    resultado = valor1 / valor2;
+                    System.out.println(valor1 + " + " + valor2 + " = " + resultado);
+                }
+                default -> {
+                    System.out.println("Entrada Invalida");
+                }
+            }
+        } else {
+            System.out.println("Entrada Invalida");
+        }
+    }
+}
