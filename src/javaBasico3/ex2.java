58fd91ab4e5bb8e972b366bb9edf048c2faf73fd
Nicollas <nsilvademoraes@gmail.com>
2024-06-06 12:18:58 -0300

    Exercicios Ex2-3

---
+++ src/javaBasico3/ex2.java
@@ -0,0 +1,22 @@
+package javaBasico3;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero, contImpar = 0, contPar = 0;
+
+        for (int i = 0; i <= 9; i++) {
+            System.out.println("Digite um número: ");
+            numero = scanner.nextInt();
+            if (numero % 2 == 0) {
+                contPar++;
+            } else  {
+                contImpar++;
+            }
+        }
+        System.out.println("Total de números pares" + contImpar);
+        System.out.println("Total de números impares" + contPar);
+    }
+}
