3c31f5d68106092ee705db93a7a350c7c72cb89a
Nicollas <nsilvademoraes@gmail.com>
2024-06-06 15:54:30 -0300

    Exercicios Ex5-3

---
+++ src/javaBasico3/ex5.java
@@ -0,0 +1,24 @@
+package javaBasico3;
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+
+        int num, contNum = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite os numeros para adicionar na soma total dos positivos (digite 0 para sair): ");
+
+        do{
+            num = sc.nextInt();
+
+            if(num > 0){
+                contNum += num;
+            }
+
+        }while(num != 0);
+
+        System.out.println("Soma total dos números positivos é: " + contNum);
+    }
+}
+
